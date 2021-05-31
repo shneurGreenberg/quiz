6d5f3d8a74ecb86f339ad0f1e86d1a0008733db0
hillel <a4105962@gmail.com>
2021-05-31 16:47:16 +0300

    cont

--- hille/src/view/components/QuizBox/QuizBox.jsx
+++ hille/src/view/components/QuizBox/QuizBox.jsx
@@ -2,30 +2,40 @@ import React, { useState, useEffect } from "react";
 let quizQestions = [
   {
     question: "מהו ההר הגבוה בעולם",
-    answer: "האוורסט",
-    answerAll: ["האוורסט", "חרמון", "גבעת התחמושת"],
+    answerAll: [{
+      text: 'חרמון',
+      num: 10,
+    }, {
+      text: 'האוורסט',
+      num: 5,
+    }, {
+      text: 'גבעת התחמושת',
+      num: 10,
+    }
+    ]
   },
   {
-    question: "איזה מתכת נוזלית בטמפרטורת החדר?",
-    answer: "גליום",
-    answerAll: ["גליום", "מגנזיום", "אלומיניום"],
-  },
+    question: "מהו ההר הנמוך בעולם",
+    answerAll: [{
+      text: 'חרמון',
+      num: 10,
+    }, {
+      text: 'האוורסט',
+      num: 5,
+    }, {
+      text: 'גבעת התחמושת',
+      num: 10,
+    }
+    ]
+  }
 ];
+
 
 let randomNumber = 0;
 
 function QuizBox() {
-  const [answers, setanswers] = useState([
-    {
-      question: "איזה מתכת נוזלית בטמפרטורת החדר?",
-      answer: "גליום",
-      answerAll: ["גליום", "מגנזיום", "אלומיניום"],
-    },
-  ]);
+  const [answers, setanswers] = useState(quizQestions[Math.floor(Math.random() * quizQestions.length)]);
 
-  useEffect(() => {
-    getQuestion();
-  }, []);
 
   function getQuestion() {
     const random = Math.floor(Math.random() * quizQestions.length);
@@ -38,19 +48,23 @@ function QuizBox() {
     }
   }
 
-  function checkAnswer() {}
-  console.log(answers[0])
 
+  function checkAnswer(elm) {
+  //   if (elm === answers.answer) {
+  //     console.log(true)
+  //   } else {
+  //     console.log(false)
+  //   }
+  }
 
   return (
     <>
       <button onClick={getQuestion}>שאלה הבאה</button>
       <div>{answers.question}</div>
-      {answers[0].answerAll.map((elm, index) => {
+      {answers.answerAll.map((elm, index) => {
         return (
-          
-          <div key={index} onClick={() => checkAnswer(elm)}>
-            {elm}
+          <div key={index} onClick={() => checkAnswer(elm.text)}>
+            {elm.text}
           </div>
         );
       })}
